714af5fbf563ab12b7f24259d66cb24d59f55809
Lulu <vannary251@gmail.com>
2021-02-24 13:08:02 -0500

    added onKeypress search functionality

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 This web app's purpose is to list all the public Github repos for a specified Github username
 
 ## Features
-- Search for public repos for a specific username
+- Search for public repos for a specific username via enter or search button
+- 
 
 ## Technical
 
 ## Credits
 - Github API: [https://docs.github.com/en/rest/reference/repos#list-repositories-for-a-user]
-- Bootstrap React [https://react-bootstrap.github.io/getting-started/introduction/]
 - Axios [https://github.com/axios/axios]
 - ReactJS [https://reactjs.org/]
 - Gitname - Deploy React app on Github pages [https://github.com/gitname/react-gh-pages]
--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
 
   function Element(){
     const [userName, setUserName] = useState()
-    const [repos, setRepos] = useState([{name:""}])
+    const [repos, setRepos] = useState()
 
     function clickHandler(e){
-    
+      
       axios.get(`https://api.github.com/users/${userName}/repos`)
       .then(response => {
         const repos_list = response.data
@@ -17,6 +17,16 @@ function App() {
         setRepos(repos_list);
         })
       }
+
+      function enterHandler(e){
+        e.keyCode == 13 ?
+          axios.get(`https://api.github.com/users/${userName}/repos`)
+          .then(response => {
+            const repos_list = response.data
+            console.log(repos_list)
+            setRepos(repos_list);
+            }) : setRepos()
+        }
 
     function handleChange(e){
       e.preventDefault()
@@ -27,13 +37,12 @@ function App() {
       <div>
         <h3>List Github public repos for a user</h3>
         <div className="container">
-        <input placeholder="username" onChange={handleChange}></input>
+        <input placeholder="username" onChange={handleChange} onKeyUp={enterHandler}></input>
         <button onClick={clickHandler}>Search</button>
         </div>
-        {repos ? <ul>
-          {repos.map( (element,i) => <li key={i}><a href={element.html_url}>{element.name}</a> developed in <span>{element.language}</span></li>)}
-        </ul> : <div></div>}
-        
+        {repos != undefined ? <ul>
+          {repos.map( (element,i) => <li key={i}><a href={element.html_url} target="_blank" rel="noreferrer">{element.name}</a> developed in <span>{element.language}</span></li>)}
+        </ul> : <div>No public repos found for that user!</div>} 
       </div>
     )
   }
